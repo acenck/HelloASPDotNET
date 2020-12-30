04815709625dea2a9bc66a0e8cbcbc6f305c3a4e
acenck <nickacinelli@gmail.com>
2020-12-30 00:15:32 -0600

    Created index and welcome view, get and post respectively, display form user inputs for both name and language for personalized greeting

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -19,64 +19,49 @@ namespace HelloASPDotNET.Controllers
             HttpContext.Request.Headers.TryGetValue("Accept", out var acceptHeaderVal);
             Console.WriteLine(acceptHeaderVal);
 
-            string html = "<form method='post' action='/helloworld/greeting'>" +
-      "<input type='text' name='name' />" +
-      "<select name='language' id='language'><option value=''>Languages</option> <option value='English'>English</option> <option value='Spanish'>Spanish</option> <option value='French'>French</option> <option value='Italian'>Italian</option> <option value='German'>German</option>" +
-            
-      "<input type='submit' value='Greet Me!' />" +
-      "</form>"; 
+           
+            return View();
 
-            return Content(html, "text/html");
         }
 
-        [HttpGet("welcome/{name?}")]
+        [HttpGet("welcome/{name?}/{language?}")]
         [HttpPost]
-        public IActionResult Welcome(string name = "World")
-        {
-            if (String.IsNullOrEmpty(name))
-            {
-                name = "World";
-            }
-
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
-        }
-
-
-
-        [HttpGet("greeting/{name?}/{language?}")]
-        [HttpPost("greeting")]
-        public IActionResult CreateMessage(string name = "Fella", string language = "English")
+        public IActionResult Welcome(string name = "Fella", string language = "English")
         {
             var languageList = new Dictionary<string, string>()
-            {
-                {"English", "Hello" },
-                {"Spanish", "Hola" },
-                {"French", "Bonjour" },
-                {"Italian", "Ciao" },
-                {"German", "Guten Tag" }
-            };
+             {
+                 {"English", "Hello" },
+                 {"Spanish", "Hola" },
+                 {"French", "Bonjour" },
+                 {"Italian", "Ciao" },
+                 {"German", "Guten Tag" }
+             };
 
+            ViewBag.person = name;
 
             if (String.IsNullOrEmpty(name))
             {
-                name = "Fella";
+                ViewBag.person = "Fella";
             }
 
             if (String.IsNullOrEmpty(language))
             {
-                language = "English";
+                ViewBag.language = "English";
             }
 
-           
-            string languageChoice = languageList[language];
-                
-
-            return Content("<h1>" + languageChoice + "," + " " + name + "!</h1>", "text/html");
-
-
+            ViewBag.language = languageList[language];
+            return View();
 
             
+
+
+           
+
         }
+
+
+
+     
         
     }
 }
